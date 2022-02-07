0ee39d8bed46d424e7b2d8e420c953512a2bc870
Lorena Morais <lorenamorais@alunos.utfpr.edu.br>
2022-02-07 17:00:30 -0300

    versão quase finalizada

--- scripts.js
+++ scripts.js
@@ -1,6 +1,12 @@
-let qtdCartas, jogadas;
-let cartasEscolhidas =  [];
-let virarCartaEscolhida = true;
+let qtdCartas = 0;
+let qtdPares = 0;
+let qtdjogadas  = 0;
+let tempo = 0;
+let time = 0;
+let cartaEscolhida = null;
+let cartaEscolhidaVirada = false;
+let paresCorretos = [];
+let relogio = document.querySelector(".reloginho");
 
 function adicionarCartas(){
     while(qtdCartas %2 !== 0 || qtdCartas < 4 || qtdCartas > 14){
@@ -15,20 +21,21 @@ function adicionarCartas(){
         'imagens/tripletsparrot.gif',
         'imagens/unicornparrot.gif'
     ];
+    qtdPares = (qtdCartas/2);
     let paresDeCartas = [];
-    for(let i = 0; i < (qtdCartas/2); i++){
+    for(let i = 0; i < qtdPares; i++){
         paresDeCartas.push(imagens[i], imagens[i]);
     }    
-    paresDeCartas.sort(comparador); // Após esta linha, a minhaArray estará embaralhada
+    paresDeCartas.sort(comparador);
     let cartas = document.querySelector("main");
     for (let i = 0; i < qtdCartas; i++){
         cartas.innerHTML += `
-            <div class="carta" data-identifier="card">
+            <div class="carta" onclick="clicarNaCarta(this)" data-identifier="card">
                 <div class="frente-carta face">
-                    <img src="imagens/front.png" data-identifier="back-face"/>
+                    <img src="imagens/front.png" alt="Papagaio bonitinho" data-identifier="front-face"/>
                  </div>
                 <div class="verso-carta face">
-                    <img src="${paresDeCartas[i]}" data-identifier="front-face"/>
+                    <img src="${paresDeCartas[i]}" data-identifier="back-face"/>
                 </div>
             </div>
         `
@@ -37,6 +44,64 @@ function adicionarCartas(){
 
 function comparador() { 
     return Math.random() - 0.5; 
+}
+
+
+function virarCarta(carta){
+    let verso = carta.querySelector(".verso-carta");
+    verso.classList.toggle("verso-escolhida");
+
+    let frente = carta.querySelector(".frente-carta");
+    frente.classList.toggle("frente-escolhida");
+}
+
+function clicarNaCarta(carta){
+    let verso = carta.querySelector(".verso-carta");
+
+    if(qtdjogadas == 0){
+        tempo = setInterval(temporizador, 1000);
+    }
+
+    if(verso.classList.contains("verso-escolhida") === false){
+        virarCarta(carta);
+        qtdjogadas++;
+        if(cartaEscolhidaVirada === false){
+            cartaEscolhida = carta;
+            cartaEscolhidaVirada = true;
+        } else if(cartaEscolhida.classList[1] !== carta.classList[1]){
+            cartaEscolhidaVirada = false;
+            setTimeout(virarCarta,1000,cartaEscolhida);
+            setTimeout(virarCarta,1000,carta);
+            cartaEscolhida = null;
+        } else{
+            cartaEscolhidaVirada = false;
+            paresCorretos.push(carta.classList[1]);
+        }
+        if(paresCorretos.length == qtdPares){
+            clearTimeout(tempo);
+            setTimeout(finalizarJogo, 1000);
+        }
+
+    }
+}
+
+function finalizarJogo(){
+    alert(`Você ganhou em ${qtdjogadas} jogadas e em ${relogio} segundos`);
+    
+    let jogarNovamente = prompt("Quer de jogar novamente? Digite 's' para sim, se não clique em qualquer tecla ou aperte enter.");
+        if (jogarNovamente === "s") {
+            window.location.reload(true);
+        }
+}
+
+
+function temporizador(){
+    time = relogio.innerHTML;
+    time++;
+}
+
+function desvirarCarta() {
+    
 }
 
 adicionarCartas();
